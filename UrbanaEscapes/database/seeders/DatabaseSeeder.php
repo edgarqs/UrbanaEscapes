1ee7bd7cf8793b7fe43330836f964a0ca3c2e466
Pol González Miranda <pgmxx04@gmail.com>
2024-12-02 17:28:16 +0100

    Creacio seeders

--- UrbanaEscapes/database/seeders/DatabaseSeeder.php
+++ UrbanaEscapes/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Habitacion;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // Para usar DB::
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       Habitacion::factory(100)->create();
+        if ($this->command->confirm('Vols refrescar la base de dades?', true)) {
+            $this->command->call('migrate:refresh');
+            $this->command->info("S'ha reconstruït la base de dades");
+        }
+
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $hotel = \App\Models\Hotel::create(['nom' => 'The Kyoto', 'adreca' => 'Carrer Mariner, 32', 'ciutat' => 'Madrid', 'pais' => 'Espanya', 'email' => 'info@thekyoto.urbanaescapes.com', 'telefon' => '934567890']);
+        $this->command->info("  + Creat hotel de proves $hotel->nom, $hotel->adreca");
+
+        $serveis = [
+            [
+                'nom' => 'Minibar',
+            ],
+            [
+                'nom' => 'Cafetera',
+            ],
+            [
+                'nom' => 'Microones',
+            ],
+            [
+                'nom' => 'Smart TV',
+            ],
+            [
+                'nom' => 'Mascotas',
+            ]
+        ];
+
+        // $serveis = array_map(fn($nom) => ['nom' => $nom], [
+        //     'Minibar', 'Cafetera', 'Microones', 'Smart TV', 'Mascotas'
+        // ]);
+
+        // Importar les dades a la base de dades
+        DB::table('serveis')->insert($serveis);
+
+        $this->command->info("  + Afegit els servis a la taula serveis");
+
+        \App\Models\Usuari::factory(50)->create();
+
+        Habitacion::factory(50)->create();
     }
 }
 
