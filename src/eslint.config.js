3560619e6638d4e36b53049e043dcf18c471cbad
Pol González Miranda <pgmxx04@gmail.com>
2024-12-10 20:28:47 +0100

    Instalacio de eslint,psalm i husky

---
+++ src/eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+       globals: globals.browser 
+      },
+    rules: {
+            "no-unused-vars": "error",
+            "no-undef": "error"
+        }
+  },
+  pluginJs.configs.recommended,
+];
