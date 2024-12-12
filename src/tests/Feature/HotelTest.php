3a99921f6bf6b77286c312162b351cea8e5c39e9
Pol González Miranda <pgmxx04@gmail.com>
2024-12-12 19:40:14 +0100

    Afegits tests dels hotels

--- src/tests/Feature/HotelTest.php
+++ src/tests/Feature/HotelTest.php
@@ -8,14 +8,32 @@ use Tests\TestCase;
 
 class HotelTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    public function test_when_create_hotel_expect_insert_hotel_in_database(): void
+    {
+        $response = $this->post('/create', [
+            'nom' => 'Hotel Test',
+            'adreca' => 'Carrer Test',
+            'ciutat' => 'Ciutat Test',
+            'pais' => 'Pais Test',
+            'email' => 'test@test.es',
+            'telefon' => '+34 123 45 67 89'
+        ]);
+
+        $response->assertStatus(302);
+    }
 
     public function test_when_create_hotel_expect_redirect_to_selector_hotels(): void
     {
-        $response=$this->get('/create');
-        $response->assertFound();
-        $response->assertRedirectToRoute('hotel.selector');
+        $response = $this->post('/create', [
+            'nom' => 'Hotel Test',
+            'adreca' => 'Carrer Test',
+            'ciutat' => 'Ciutat Test',
+            'pais' => 'Pais Test',
+            'email' => 'test@test.es',
+            'telefon' => '+34 123 45 67 89'
+        ]);
+
+        $response->assertRedirect('/');
+        $response->assertStatus(302);
     }
 }
