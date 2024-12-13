3e59a10ce52c1f7f02c377d55410cd869bddb281
Edgar Quirante <quirante.sanchez.edgar@alumnat.copernic.cat>
2024-12-13 17:10:08 +0100

    Actualitzat diagrama sql

--- docs/urbanaescapes.sql
+++ docs/urbanaescapes.sql
@@ -20,13 +20,24 @@ CREATE TABLE habitacions (
     llits_supletoris ENUM('0','1','2') NOT NULL,
     preu DECIMAL(10, 2) NOT NULL,
     numHabitacio INT NOT NULL,
+    estat ENUM('lliure', 'ocupada') NOT NULL,
     FOREIGN KEY (hotel_id) REFERENCES hotels(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rols (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE usuaris (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    rol_id INT NOT NULL,
+    hotel_id INT,
+    FOREIGN KEY (rol_id) REFERENCES rols(id) ON DELETE CASCADE,
+    FOREIGN KEY (hotel_id) REFERENCES hotels(id) ON DELETE CASCADE
 );
 
 CREATE TABLE reservas (
@@ -36,7 +47,8 @@ CREATE TABLE reservas (
     data_entrada DATE NOT NULL,
     data_sortida DATE NOT NULL,
     preu_total DECIMAL(10, 2) NOT NULL,
-    estat ENUM('pend_checkin', 'pend_checkout', 'cancelada', 'finalizada') NOT NULL,
+    estat ENUM('reservada', 'checkin', 'checkout', 'cancelada') NOT NULL,
+    comentaris VARCHAR(255),
     FOREIGN KEY (habitacion_id) REFERENCES habitacions(id) ON DELETE CASCADE,
     FOREIGN KEY (usuari_id) REFERENCES usuaris(id) ON DELETE CASCADE
 );
