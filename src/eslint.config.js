58a5769cae0c418b973a26e58758fda325dd48d1
Edgar Quirante <quirante.sanchez.edgar@alumnat.copernic.cat>
2025-01-20 20:43:08 +0100

    Hooks subido porfin

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,18 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["*.js"],
-    languageOptions: {
-       globals: globals.browser 
-      },
-    rules: {
+    {
+        files: ["*.js"],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        rules: {
             "prefer-const": "warn",
-            "no-constant-binary-expression": "error"
-        }
-  },
-  pluginJs.configs.recommended,
+            "no-constant-binary-expression": "error",
+            "document": "off",
+        },
+        env: {
+            "document": true,
+            browser: true,
+            commonjs: true,
+            es2021: true,
+        },
+    },
+    pluginJs.configs.recommended,
 ];
