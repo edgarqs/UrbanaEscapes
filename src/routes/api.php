48be21f9733a4d6624997ca706e08a33a8afd971
Edgar Quirante <quirante.sanchez.edgar@alumnat.copernic.cat>
2025-01-21 16:08:07 +0100

    Modificació APIs

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Api\UsuariController;
 use App\Http\Controllers\Api\ReservesController;
 use App\Http\Controllers\Api\HotelController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('v1')->group(function () {
     Route::apiResource('/usuaris', UsuariController::class);
     Route::apiResource('/reserves', ReservesController::class);
