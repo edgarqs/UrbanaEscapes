4a4c79878ff507a3ee39d37fc9d30ce05fed2a55
pgonzalez <gonzalez.miranda.pol@alumnat.copernic.cat>
2025-01-27 20:17:53 +0100

    Afegit el footer

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -4,8 +4,11 @@ import App from './components/Hero.vue';
 import Header from './components/Header.vue';
 import Vcorpo from './components/ValorsCorporatius.vue';
 import hotels from './components/Hotels.vue';
+import footer from './components/Footer.vue';
 
 createApp(Header).mount('#header');
 createApp(App).mount('#hero');
 createApp(Vcorpo).mount('#valorscorporatius');
 createApp(hotels).mount('#hotels');
+createApp(footer).mount('#footer');
+
