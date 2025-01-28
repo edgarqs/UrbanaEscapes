62897751de8b7b6f6daa49e0be034c5a03d77302
Edgar QS <quirante.sanchez.edgar@alumnat.copernic.cat>
2025-01-28 18:25:17 +0100

    Comenzada página habitacions lliures #73

---
+++ web/resources/views/habitacions.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Habitacions | uEscapes</title>
+    <link rel="icon" href="{{ asset('img/urbana.ico') }}" type="image/x-icon">
+    @vite(['resources/css/main.scss'])
+</head>
+
+<body>
+
+    <div id="header" class="m-auto"></div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
