d563fc3065d12cfa8143f6d97fd9a5cd33e45a48
pgonzalez <gonzalez.miranda.pol@alumnat.copernic.cat>
2025-01-28 16:41:01 +0100

    fet canvis a la view de ofertas

--- web/resources/js/components/Ofertas.vue
+++ web/resources/js/components/Ofertas.vue
@@ -1,12 +1,28 @@
 <style scoped>
-.container {
-    background-image: url("./img/images.jpeg");
+h1 {
+    text-align: center;
+    margin: 20px 0;
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 1.5;
+    color: #333;
+    margin-bottom: 1rem;
 }
 </style>
 
 <template>
-    <div class="container">
-        <h1>Ofertas</h1>
+    <h1>Ofertas</h1>
+    <div class="container flex items-center justify-center">
+        <div>
+            <img src="./img/urbana_logo-sinFondo.avif" alt="Logo" class="">
+        </div>
+        <div>
+            <h2>25% DESCOMPTE</h2>
+            <h2>25% DESCOMPTE</h2>
+        </div>
+        <div>
+
+        </div>
     </div>
 </template>
 
