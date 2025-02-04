b145becab59bda0563647903884fab16726be9b7
Pol González Miranda <pgmxx04@gmail.com>
2025-02-04 18:03:37 +0100

    Afegida la landing page

---
+++ web-vue/src/components/HeroSection.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
+
+<template>
+  <video loop muted autoplay playsinline poster="" class="background-video">
+    <source src="./../video/videohotel.mp4" type="video/mp4" />
+  </video>
+</template>
+
+<style scoped>
+.background-video {
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.page-hero {
+  position: relative;
+  overflow: hidden;
+}
+
+video[poster] {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
