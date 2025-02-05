df147a21ca0037c526d596af3bd6d54f8548a463
Edgar QS <quirante.sanchez.edgar@alumnat.copernic.cat>
2025-02-05 16:50:46 +0100

    Creo q solucionado el error de la página q no se ve

--- src/app/Models/Reservas.php
+++ src/app/Models/Reservas.php
@@ -33,8 +33,94 @@ class Reservas extends Model
         return $this->belongsTo(Habitacion::class);
     }
 
+    public function serveis()
+    {
+        return $this->belongsToMany(Serveis::class, 'reservas_serveis', 'reservas_id', 'serveis_id');
+    }
+
     public function usuari()
     {
         return $this->belongsTo(Usuari::class);
+    }
+
+    // Habitacions ocupades
+    public static function countHabitacionesConfirmadas($hotelId)
+    {
+        $count = Habitacion::where('estat', 'Ocupada')->where('hotel_id', $hotelId)->count();
+        Log::channel('info_log')->info('Comptador d\'habitacions confirmades', ['hotel_id' => $hotelId, 'count' => $count]);
+        return $count;
+    }
+
+    // Habitacions lliures
+    public static function countHabitacionesLliures($hotelId)
+    {
+        $count = Habitacion::where('estat', 'Lliure')->where('hotel_id', $hotelId)->count();
+        Log::channel('info_log')->info('Comptador d\'habitacions lliures', ['hotel_id' => $hotelId, 'count' => $count]);
+        return $count;
+    }
+
+    public static function countHabitacionesBloquejades($hotelId)
+    {
+        $count = Habitacion::where('estat', 'Bloquejada')->where('hotel_id', $hotelId)->count();
+        Log::channel('info_log')->info('Comptador d\'habitacions bloquejades', ['hotel_id' => $hotelId, 'count' => $count]);
+        return $count;
+    }
+
+    // Reserves amb estat "reservada"
+    public static function countReservasPendientes($hotelId)
+    {
+        $count = Reservas::whereHas('habitacion', function ($query) use ($hotelId) {
+            $query->where('hotel_id', $hotelId);
+        })->where('estat', 'Reservada')->count();
+
+        Log::channel('info_log')->info('Comptador de reserves pendents', ['hotel_id' => $hotelId, 'count' => $count]);
+        return $count;
+    }
+
+    // Totes les habitacions
+    public static function getHabitacionesTotals($hotelId)
+    {
+        $habitacions = Habitacion::where('hotel_id', $hotelId)->get()->count();
+        Log::channel('info_log')->info('Comptador de totes les habitacions', ['hotel_id' => $hotelId, 'count' => $habitacions]);
+        return $habitacions;
+    }
+
+    public static function getCheckinsPendents($hotelId)
+    {
+        return Reservas::whereHas('habitacion', function ($query) use ($hotelId) {
+            $query->where('hotel_id', $hotelId);
+        })
+            ->where('estat', 'Reservada')
+            ->orderBy('id')
+            ->get();
+    }
+
+    public static function calcularPreuPerDies($habitacio, $diaActual, $diaSeguent)
+    {
+        $preuPerDies = 0;
+        $dataInici = new Datetime($diaActual);
+        $dataFi = new Datetime($diaSeguent);
+        $dies = $dataInici->diff($dataFi)->days;
+        $preuPerDies = $habitacio->preu * $dies;
+
+        return $preuPerDies;
+    }
+
+    public static function calcularPreuTotal($serveis, $habitacio, $diaInicial, $diaFinal)
+    {
+        $preuTotal = 0;
+        if (is_string($habitacio)) {
+            $habitacio = Habitacion::find($habitacio);
+        }
+        $preuPerDia = $habitacio->preu;
+        $dataInici = new Datetime($diaInicial);
+        $dataFi = new Datetime($diaFinal);
+        $diesTotal = $dataInici->diff($dataFi)->days;
+        foreach ($serveis as $serveiId) {
+            $servei = Serveis::find($serveiId);
+            $preuPerDia += $servei->preu;
+        }
+        $preuTotal += $preuPerDia * $diesTotal;
+        return $preuTotal;
     }
 }
