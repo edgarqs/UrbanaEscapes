ea3488b1fd706e955e03c137643f18b3ccc736a2
Pol González Miranda <pgmxx04@gmail.com>
2025-02-10 21:15:58 +0100

    Feta verificacio per mail

--- web/src/components/HabitacionsSection.vue
+++ web/src/components/HabitacionsSection.vue
@@ -1,42 +1,5 @@
-<template>
-    <div>
-        <h1 class="text-3xl font-bold text-center mt-16 mb-8 text-gray-800">Habitacions Disponibles</h1>
-        <div class="container mx-auto p-4">
-            <div v-for="(habitacions, hotelId) in groupedHabitacions" :key="hotelId" class="mb-8 p-4 border border-gray-300 rounded-lg bg-gray-50 max-w-6xl mx-auto">
-                <div class="mb-4">
-                    <h2 class="text-2xl font-bold text-gray-800">{{ habitacions[0].hotel.nom }}</h2>
-                    <p class="text-sm text-gray-600">{{ habitacions[0].hotel.adreca }}, {{ habitacions[0].hotel.ciutat }}, {{ habitacions[0].hotel.pais }}</p>
-                </div>
-                <div class="w-full flex flex-wrap gap-6">
-                    <div v-for="habitacio in habitacions" :key="habitacio.id" class="w-full flex bg-white shadow-lg rounded-lg overflow-hidden">
-                        <div class="flex-shrink-0 w-1/3 h-56 overflow-hidden">
-                            <!-- Cambiar la imagen basada en el tipo de habitación -->
-                            <img :src="getImageForRoomType(habitacio.tipus)"
-                                 :alt="`Habitación ${habitacio.id}`" class="w-full h-full object-cover transition-transform transform hover:scale-110">
-                        </div>
-                        <div class="p-4 flex flex-col justify-between w-2/3">
-                            <h5 class="text-xl font-semibold">Habitació {{ habitacio.tipus }}</h5>
-                            <p class="text-sm text-gray-700"><strong>Descripció: </strong>Habitació {{ habitacio.tipus }} amb {{ habitacio.llits }} llits y {{ habitacio.llits_supletoris }} llits supletoris.<span v-if="habitacio.id % 2 === 0"> Balconada amb vistes exteriors.</span></p>
-                            <p class="text-sm text-gray-700"><strong>Capacitat:</strong> {{ habitacio.llits + habitacio.llits_supletoris }} persones</p>
-                            <div class="flex justify-between items-center">
-                                <span class="text-xl font-bold text-orange-500">
-                                    {{ (habitacio.preu * numNights * numPeople).toFixed(2) }} €
-                                </span>
-                                <p class="text-s text-gray-500">(Preu per {{ numNights }} nits i {{ numPeople }} persones)</p>
-                            </div>
-                            <button class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mt-4">Reservar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="error-message" class="alert alert-danger hidden bg-red-100 text-red-700 p-4 rounded-lg mt-4" role="alert">
-                No se ha podido conectar con el servidor. Por favor, inténtelo más tarde.
-            </div>
-        </div>
-    </div>
-</template>
-
 <script>
+
 export default {
     data() {
         return {
@@ -112,11 +75,16 @@ export default {
             // Imagen por defecto si no se encuentra el tipo de habitación
             return 'https://i.postimg.cc/d3V2vht7/default-room.jpg';
         },
-        
+
         filterHabitacions(query) {
             const { destination, startDate, endDate, people } = query;
             const start = new Date(startDate);
             const end = new Date(endDate);
+
+            // Guardar las fechas en localStorage
+            localStorage.setItem('startDate', query.startDate);
+            localStorage.setItem('endDate', query.endDate);
+            localStorage.setItem('numPeople', query.people);
 
             fetch('http://localhost:8000/api/v1/reserves')
                 .then(response => response.json())
@@ -167,7 +135,7 @@ export default {
                     }, {});
                 });
         },
-        
+
         matchDestination(habitacio, destination) {
             const searchString = destination.trim().toLowerCase();
             const hotelDetails = [
@@ -189,3 +157,55 @@ export default {
     }
 };
 </script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-center mt-16 mb-8 text-gray-800">Habitacions Disponibles</h1>
+        <div class="container mx-auto p-4">
+            <div v-for="(habitacions, hotelId) in groupedHabitacions" :key="hotelId"
+                class="mb-8 p-4 border border-gray-300 rounded-lg bg-gray-50 max-w-6xl mx-auto">
+                <div class="mb-4">
+                    <h2 class="text-2xl font-bold text-gray-800">{{ habitacions[0].hotel.nom }}</h2>
+                    <p class="text-sm text-gray-600">{{ habitacions[0].hotel.adreca }}, {{ habitacions[0].hotel.ciutat
+                        }}, {{ habitacions[0].hotel.pais }}</p>
+                </div>
+                <div class="w-full flex flex-wrap gap-6">
+                    <div v-for="habitacio in habitacions" :key="habitacio.id"
+                        class="w-full flex bg-white shadow-lg rounded-lg overflow-hidden">
+                        <div class="flex-shrink-0 w-1/3 h-56 overflow-hidden">
+                            <!-- Cambiar la imagen basada en el tipo de habitación -->
+                            <img :src="getImageForRoomType(habitacio.tipus)" :alt="`Habitación ${habitacio.id}`"
+                                class="w-full h-full object-cover transition-transform transform hover:scale-110">
+                        </div>
+                        <div class="p-4 flex flex-col justify-between w-2/3">
+                            <h5 class="text-xl font-semibold">Habitació {{ habitacio.tipus }}</h5>
+                            <p class="text-sm text-gray-700"><strong>Descripció: </strong>Habitació {{ habitacio.tipus
+                                }} amb {{ habitacio.llits }} llits y {{ habitacio.llits_supletoris }} llits
+                                supletoris.<span v-if="habitacio.id % 2 === 0"> Balconada amb vistes exteriors.</span>
+                            </p>
+                            <p class="text-sm text-gray-700"><strong>Capacitat:</strong> {{ habitacio.llits +
+                                habitacio.llits_supletoris }} persones</p>
+                            <div class="flex justify-between items-center">
+                                <span class="text-xl font-bold text-orange-500">
+                                    {{ (habitacio.preu * numNights * numPeople).toFixed(2) }} €
+                                </span>
+                                <p class="text-s text-gray-500">(Preu per {{ numNights }} nits i {{ numPeople }}
+                                    persones)</p>
+                            </div>
+
+
+                            <button
+                                class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mt-4">
+                                <RouterLink :to="`/reserva/${habitacio.id}`" class="text-white">Reservar</RouterLink>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="error-message" class="alert alert-danger hidden bg-red-100 text-red-700 p-4 rounded-lg mt-4"
+                role="alert">
+                No se ha podido conectar con el servidor. Por favor, inténtelo más tarde.
+            </div>
+        </div>
+    </div>
+</template>
---
+++ web/src/views/ReservaView.vue
@@ -0,0 +1,155 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+import emailjs from 'emailjs-com';
+import Header from '../components/HeaderSection.vue';
+import Footer from '../components/FooterSection.vue';
+
+const habitacio = ref(null);
+
+const route = useRoute();
+const router = useRouter();
+
+const startDate = ref(localStorage.getItem('startDate'));
+const endDate = ref(localStorage.getItem('endDate'));
+const diesTotals = Math.ceil((new Date(endDate.value) - new Date(startDate.value)) / (1000 * 60 * 60 * 24));
+
+onMounted(() => {
+  fetch(`http://localhost:8000/api/v1/habitacions/${route.params.id}`)
+    .then(response => response.json())
+    .then(data => {
+      habitacio.value = data;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+});
+
+const generateToken = Math.floor(100000 + Math.random() * 900000);
+
+const nom = ref('');
+const email = ref('');
+const token = ref(generateToken);
+
+// Manejo de visibilidad de los divs
+const mostrarReserva = ref(true);
+const mostrarVerificacio = ref(false);
+const codigoIngresado = ref('');
+const errorMensaje = ref('');
+
+const sendEmail = () => {
+  const templateParams = {
+    to_name: nom.value,
+    to_email: email.value,
+    token: token.value
+  };
+
+  emailjs.send('service_ibvhq01', 'template_h2287v9', templateParams, 'RZvzJ3Yx8E4c1O3tK')
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      mostrarReserva.value = false;
+      mostrarVerificacio.value = true;
+    }, (error) => {
+      console.error('FAILED...', error);
+    });
+};
+
+const verificarCodigo = () => {
+  if (codigoIngresado.value == token.value) {
+    router.push(`/compra/${habitacio.value.id}`);
+  } else {
+    errorMensaje.value = 'El código no es correcto. Inténtelo de nuevo.';
+  }
+};
+</script>
+
+<template>
+  <main>
+    <Header />
+    <RouterLink to="/habitacions" class="tornar text-blue-500">Tornar</RouterLink>
+
+    <div v-if="mostrarReserva" class="reservas flex flex-col m-4 justify-center items-center">
+      <div class="flex justify-between p-4 px-8">
+        <div class="detallsReserva">
+          <div class="reserva" v-if="habitacio">
+            <h2 class="text-2xl font-bold">Habitació {{ habitacio.tipus }}</h2>
+            <p><strong>Descripció:</strong> Habitació {{ habitacio.tipus }} amb {{ habitacio.llits }} llits y {{
+              habitacio.llits_supletoris }} llits supletoris.</p>
+            <p><strong>Capacitat:</strong> {{ habitacio.llits + habitacio.llits_supletoris }} persones</p>
+            <p><strong>Preu total:</strong> {{ habitacio.preu * diesTotals }} €</p>
+            <p><strong>Data d'entrada:</strong> {{ startDate }}</p>
+            <p><strong>Data de sortida:</strong> {{ endDate }}</p>
+          </div>
+          <div v-else>
+            <p>Carregant dades de la habitació...</p>
+          </div>
+        </div>
+
+        <div class="detallsClient">
+          <form @submit.prevent="sendEmail">
+            <h2 class="text-2xl font-bold">Dades del client</h2>
+            <div class="form-group">
+              <label for="to_name">Nom:</label>
+              <input type="text" id="to_name" v-model="nom" required>
+            </div>
+            <div class="form-group">
+              <label for="to_email">Correu electrònic:</label>
+              <input type="email" id="to_email" v-model="email" required>
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="mostrarVerificacio" class="verificacio flex flex-col items-center">
+      <h2 class="text-2xl font-bold">Verificació</h2>
+      <p>Un correu electrònic ha estat enviat a {{ email }} amb un codi de verificació.</p>
+      <p>Si us plau, introdueixi el codi de verificació per confirmar la seva reserva.</p>
+
+      <input type="text" v-model="codigoIngresado" required>
+      <button @click="verificarCodigo">Confirmar reserva</button>
+
+      <p v-if="errorMensaje" class="text-red-500 mt-2">{{ errorMensaje }}</p>
+    </div>
+
+    <Footer />
+  </main>
+</template>
+
+<style scoped>
+.detallsReserva,
+.detallsClient {
+  width: 50%;
+}
+
+.form-group {
+  margin-bottom: 1rem;
+}
+
+.tornar {
+  margin-top: 1rem;
+}
+
+input {
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  background-color: #3182ce;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #2c5282;
+}
+
+.text-red-500 {
+  color: red;
+}
+</style>
